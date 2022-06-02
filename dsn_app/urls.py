2cada6b7ee3edb7d11e771ba47c0f94f3723a033
User <giwayusuf12@gmail.com>
2022-06-02 22:55:52 +0100

    merging

--- dsn_app/urls.py
+++ dsn_app/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('user_app.api.urls')),
     path('test/', include('user_app.urls'))
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-
+]
