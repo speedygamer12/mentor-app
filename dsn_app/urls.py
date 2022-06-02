5c7b982a773a5a00ece01ce0d4845b3e07eff4d4
User <giwayusuf12@gmail.com>
2022-06-02 23:05:11 +0100

    clean-up

--- dsn_app/urls.py
+++ dsn_app/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from student_management_system import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
