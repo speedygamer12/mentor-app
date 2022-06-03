e881d5477273d7b66b023c0ee920787f3b86f58f
User <giwayusuf12@gmail.com>
2022-06-03 23:56:27 +0100

    yasg integration

--- dsn_app/settings.py
+++ dsn_app/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework_swagger',
+    'drf_yasg',
     'rest_framework',
     'user_app',
     'rest_framework.authtoken',
